91a2827f0fddf4963ce9bf8db9e6eba95af7256b
Carlos Garcia <chenzhen.dev@gmail.com>
2025-01-10 11:57:23 -0300

    feat(integration): integration weahter data api for each Brazil states

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,7 +1,40 @@
-// import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 
+import { brazilStates } from './data'
+
 function App() {
+
+  const {
+    VITE_API_KEY, 
+    VITE_API_BASE_URL, 
+  } = import.meta.env
+
+  useEffect(() => {
+      
+    (async function(){
+
+      try {
+        const promiseList = await Promise
+        .allSettled(Object.values(brazilStates)
+        .map(capital => 
+          fetch(`${VITE_API_BASE_URL}/current.json?key=${VITE_API_KEY}&q=${capital}`)
+        ))
+
+        const promiseResolvedList = promiseList
+          .filter(promise => promise.status === 'fulfilled')
+          .map(promise => promise.value.json())
+
+        const responseList = await Promise.all(promiseResolvedList)
+        console.log(responseList)
+
+      } catch (error) {
+        //console.log('=> error:', error)
+      }
+
+    }())
+
+  }, [])
   
   return (
     <>
